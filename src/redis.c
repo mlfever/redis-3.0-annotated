44695fea3eaa2da0ae039f0e47348b23cc697de8
Joseph Wang <Joseph.Wang@calix.com>
2018-06-06 17:12:07 +0800

    mem comment

--- src/redis.c
+++ src/redis.c
@@ -3744,6 +3744,13 @@ int freeMemoryIfNeeded(void) {
 
 #ifdef __linux__
 int linuxOvercommitMemoryValue(void) {
+    /*
+     *   /proc/sys/vm/overcommit_memory 可选值：0、1、2。 
+     *   0， 表示内核将检查是否有足够的可用内存供应用进程使用；如果有足够的可用内存，内存申请允许；
+     *       否则，内存申请失败，并把错误返回给应用进程。 
+     *   1， 表示内核允许分配所有的物理内存，而不管当前的内存状态如何。 
+     *   2， 表示内核允许分配超过所有物理内存和交换空间总和的内存
+     */
     FILE *fp = fopen("/proc/sys/vm/overcommit_memory","r");
     char buf[64];
 
